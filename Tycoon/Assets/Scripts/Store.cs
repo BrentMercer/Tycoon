f1bc55551bc3895e7e9e36c7ce64aa7a1a43bba2
Brent Mercer <hicx76@hotmail.com>
2019-08-25 14:26:02 -0600

    Storefront gameplay

---
+++ Tycoon/Assets/Scripts/Store.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Store : MonoBehaviour {
+
+    private int StoreCount;
+    
+    private float CurrentBalance;
+    private float BaseStoreCost;
+
+    public Text StoreCountText;
+    public Text CurrentBalanceText;
+
+    void Start () {
+        StoreCount = 1;
+        CurrentBalance = 5.00f;
+        CurrentBalanceText.text = CurrentBalance.ToString("C2");
+        BaseStoreCost = 1.50f;
+    }
+	
+	void Update () {
+
+	} 
+
+    public void BuyStoreOnClick()
+    {
+        if (BaseStoreCost > CurrentBalance)
+        {
+            return;
+        }
+        else
+        {
+            CurrentBalance -= BaseStoreCost;
+            StoreCount++;
+            StoreCountText.text = StoreCount.ToString();
+            CurrentBalanceText.text = CurrentBalance.ToString("C2");
+        }
+        
+        
+    }
+
+}
