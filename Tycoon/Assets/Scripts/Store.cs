599674ae81eb01396645aa255e1ec0c6784903e3
Brent Mercer <hicx76@hotmail.com>
2019-09-06 11:46:44 -0600

    Made income generator and store timer

--- Tycoon/Assets/Scripts/Store.cs
+++ Tycoon/Assets/Scripts/Store.cs
@@ -12,17 +12,39 @@ public class Store : MonoBehaviour {
 
     public Text StoreCountText;
     public Text CurrentBalanceText;
+    public Slider ProgressSlider;
+
+    private float StoreTimer = 4f;
+    private float CurrentTimer = 0;
+    private float BaseStoreProfit;
+
+    private bool StartTimer;
 
     void Start () {
         StoreCount = 1;
         CurrentBalance = 5.00f;
         CurrentBalanceText.text = CurrentBalance.ToString("C2");
         BaseStoreCost = 1.50f;
+        BaseStoreProfit = .5f;
+        StartTimer = false;
     }
 	
 	void Update () {
+        if (StartTimer)
+        {
+            CurrentTimer += Time.deltaTime;
+            if (CurrentTimer > StoreTimer)
+            {
+                Debug.Log("Reset start timer.");
+                StartTimer = false;
+                CurrentTimer = 0f;
+                CurrentBalance += BaseStoreProfit * StoreCount;
+                CurrentBalanceText.text = CurrentBalance.ToString("C2");
+            }
+        }
+        ProgressSlider.value = CurrentTimer / StoreTimer;
 
-	} 
+    } 
 
     public void BuyStoreOnClick()
     {
@@ -37,7 +59,15 @@ public class Store : MonoBehaviour {
             StoreCountText.text = StoreCount.ToString();
             CurrentBalanceText.text = CurrentBalance.ToString("C2");
         }
-        
+    }
+
+    public void StoreOnClick()
+    {
+        Debug.Log("Clicked the store.");
+        if (!StartTimer)
+        {
+            StartTimer = true;
+        }
         
     }
 
